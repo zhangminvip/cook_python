572508e7bbb899fb31cf0d46add9c7c5cc58ee7c
min.zhang <a991391009@gmail.com>
2018-10-23 01:18:45 +0800

    Modify semaphore test logic

--- chapter12/section_2_sema.py
+++ chapter12/section_2_sema.py
@@ -4,24 +4,30 @@ import time
 def worker(n, sema):
     # Wait to be signaled
     sema.acquire()
-
+    time.sleep(2)
     # Do some work
     print('Working', n)
+    sema.release()
 
-def run(sema):
-    while True:
-        time.sleep(2)
-        sema.release()
+# def run(sema):
+#     while True:
+#         time.sleep(2)
+#         sema.release()
+#         sema.release()
+#         sema.release()
+#         sema.release()
+
 
 # Create some threads
 
-sema = threading.Semaphore(0)
+sema = threading.Semaphore(5)
 
 nworkers = 10
 for n in range(nworkers):
     t = threading.Thread(target=worker, args=(n, sema,))
     t.start()
 
-t2 = threading.Thread(target=run, args=(sema,))
-t2.start()
+# t2 = threading.Thread(target=run, args=(sema,))
+# t2.start()
+
 
