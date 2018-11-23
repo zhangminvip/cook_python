e057c61ef4771cced7a2fc7c938c9865f081875c
min.zhang <a991391009@gmail.com>
2018-11-23 23:44:44 +0800

    test stdout

---
+++ chapter_fourteen/module_test.py
@@ -0,0 +1,15 @@
+from io import StringIO
+from unittest import TestCase
+from unittest.mock import patch
+import mymodule
+
+class TestURLPrint(TestCase):
+    def test_url_gets_to_stdout(self):
+        protocol = 'http'
+        host = 'www'
+        domain = 'example.com'
+        excepted_url = '{}://{}.{}\n'.format(protocol, host, domain)
+
+        with patch('sys.stdout', new = StringIO()) as fake_out:
+            mymodule.urlprint(protocol, host, domain)
+            self.assertEqual(fake_out.getvalue(), excepted_url)
---
+++ chapter_three/round_test.py
@@ -0,0 +1,8 @@
+import sys
+import requests
+import str_parse_time
+print(sys.path)
+ex_a = round(1.21, 1)
+requests.get('https://api.github.com/events')
+
+print('ss')
