322756f41de01d3e46e069bf0da080669f48a1d2
min.zhang <a991391009@gmail.com>
2018-11-25 23:52:44 +0800

    unix tail grep

---
+++ reference_book/chapter_one/generator.py
@@ -0,0 +1,22 @@
+# tail a file
+import time
+def tail(f):
+    f.seek(0,2)
+    while True:
+        line = f.readline()
+        if not line:
+            time.sleep(0.1)
+            continue
+        yield line
+
+def grep(lines, searchtext):
+    for line in lines:
+        if searchtext in line:
+            yield line
+
+
+# UNIX 'tail -f | grep python'
+wwwlog = tail(open('access-log'))
+pylines = grep(wwwlog,'python')
+for line in pylines:
+    print(line)
