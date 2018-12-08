b1eb9fb7898e5ae09b352cbb8c572f267a8de3d9
min.zhang <a991391009@gmail.com>
2018-12-08 19:58:57 +0800

    user of compress function

--- chapter_one/section_16.py
+++ chapter_one/section_16.py
@@ -23,3 +23,22 @@ print(ivals)
 clip_neg = [n if n >0 else 0 for n in mylist]
 print(clip_neg)
 
+
+
+from itertools import compress
+addresses = [
+    '5412 N CLARK',
+    '5148 N CLARK',
+    '5800 E 58TH',
+    '2122 N CLARK',
+    '5645 N RAVENSWOOD',
+    '1060 W ADDISON',
+    '4801 N BROADWAY',
+    '1039 W GRANVILLE',
+]
+
+counts = [ 0, 3, 10, 4, 1, 7, 6, 1]
+# more5 = [count for count in counts if count > 5]
+more5 = [n > 5 for n in counts]
+print(more5)
+print(list(compress(addresses, more5)))
