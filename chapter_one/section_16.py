411b85fadeab482a85f3e980f69f97fdcb7b9df0
min.zhang <a991391009@gmail.com>
2018-12-08 19:38:02 +0800

    use of the fileter function: filter integers in a list

--- chapter_one/section_16.py
+++ chapter_one/section_16.py
@@ -1,9 +1,22 @@
 mylist = [1, 4, -5, 10, -7, 2, 3, -1]
-list =[n for n in mylist if n >0]
+l =[n for n in mylist if n >0]
 print(list)
 
 
 # if you want to save memory
 pos = (n for n in mylist if n > 0)
+
 for p in pos:
     print(p)
+
+# more complex filtering rules
+values = ['1', '2', '-3', '-', '4', 'N/A', '5']
+def is_int(val):
+    try:
+        x = int(val)
+        return True
+    except ValueError:
+        return False
+ivals =  list(filter(is_int, values))
+print(ivals)
+
