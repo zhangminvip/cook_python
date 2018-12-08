c376d6915d86731f7369b68ac29a43cfed79b790
min.zhang <a991391009@gmail.com>
2018-12-08 20:05:48 +0800

    name the slice

--- chapter_one/section_16.py
+++ chapter_one/section_16.py
@@ -42,3 +42,15 @@ counts = [ 0, 3, 10, 4, 1, 7, 6, 1]
 more5 = [n > 5 for n in counts]
 print(more5)
 print(list(compress(addresses, more5)))
+
+
+######    0123456789012345678901234567890123456789012345678901234567890'
+record = '....................100 .......513.25 ..........'
+cost = int(record[20:23]) * float(record[31:37])
+
+
+SHARES = slice(20, 23)
+PRICE = slice(31, 37)
+cost = int(record[SHARES]) * float(record[PRICE])
+
+print(cost)
