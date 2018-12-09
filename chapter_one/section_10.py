02d35ff889054e1ea419da8290db852bf3eb0c48
min.zhang <a991391009@gmail.com>
2018-12-09 21:25:05 +0800

    the key parameter specifies a function that convert a sequence element to a hashable type

--- chapter_one/section_10.py
+++ chapter_one/section_10.py
@@ -8,3 +8,16 @@ def dedupe(items):
 
 a = [1, 5, 3, 3, 10, 7, 8, 7]
 print(list(dedupe(a)))
+
+
+def dedupe2(items, key=None):
+    seen = set()
+    for item in items:
+        val = item if key is None else key(item)
+        if val not in seen:
+            yield item
+            seen.add(val)
+
+
+a = [{'x': 1, 'y': 2}, {'x': 1, 'y': 3}, {'x': 1, 'y': 2}, {'x': 2, 'y': 4}]
+print(list(dedupe2(a, key=lambda d: (d['x'], d['y']))))
