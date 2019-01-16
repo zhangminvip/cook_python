f4456cc83598ecbc4642fe12b20f7e6b84d5c291
min.zhang <a991391009@gmail.com>
2019-01-16 10:20:43 +0800

    complete the _create_rectangle function

--- python_in_practice/diagram1.py
+++ python_in_practice/diagram1.py
@@ -33,7 +33,17 @@ class Diagram:
 
 
 def _create_rectangle(width, height, fill):
-    
+    rows = [[fill for _ in range(width)] for _ in range(height)]
+    for x in range(1, width -1):
+        rows[0][x] = HORIZONTAL
+        rows[height - 1][x] = HORIZONTAL
+    for y in range(1, height - 1):
+        rows[y][0] = VERTICAL
+        rows[y][width - 1] = VERTICAL
+    for y, x in (0, 0), (0, width - 1), (height - 1, 0), \
+                (height - 1, width - 1):
+        rows[y][x] = CORNER
+    return rows
 
 if __name__ == '__main__':
     main()
