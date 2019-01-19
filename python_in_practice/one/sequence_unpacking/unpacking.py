c688fd03522c993833a93584c6430f16912b3c17
min.zhang <a991391009@gmail.com>
2019-01-19 10:22:51 +0800

    add more content

--- python_in_practice/one/sequence_unpacking/unpacking.py
+++ python_in_practice/one/sequence_unpacking/unpacking.py
@@ -14,12 +14,19 @@ def f(x, y, fontsize):
 # f(*args)
 
 
-def ff(*args, **kwargs):
+def unpack(a):
+    print (a)
+
+def ff(*args, **kwargs): # not pass dict or tuple
     print(args)
     print(kwargs)
     print('ff:', locals())
+    # unpack(*args)     # if you pass dict and tuple ,this is result
     # f(**locals()) #error
+    f(*args, **kwargs)
 
 
-ff(1,1,2,3,fontsize = 'sdlfj', y = 'sldjf', x = 'sdlfkjj', z = 'sldkfj')
-
+# ff(1,1,2,3,fontsize = 'sdlfj', y = 'sldjf', x = 'sdlfkjj', z = 'sldkfj')
+# ff((2,3,4))   # if you pass dict and tuple ,this is result
+# ff(1,2, fontsize=100,sie=100)  # 参数和f函数的参数匹配，不能多也不能少
+ff(1,2,3, fontsize=100)  #TypeError: f() got multiple values for argument 'fontsize'
