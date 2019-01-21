de3a41062cddb3d693f8f36d4b6f8104909e8b0d
min.zhang <a991391009@gmail.com>
2019-01-21 16:28:36 +0800

    getrefcount

--- reference_book/chapter_three/garbage_collection.py
+++ reference_book/chapter_three/garbage_collection.py
@@ -1,11 +1,15 @@
 import sys
 
 a = 37
-b = 37
-# b = a
-c = []
-c.append(b)
-del c
-b = 23
+# b = 37
+# # b = a
+# c = []
+# c.append(b)
+# del c
+# b = 23
+b = {}
+b['a'] = a
+del b
 
 print(sys.getrefcount(a))
+
