a6cdefa91e1d99757b9ae9f71473305efc35e93a
min.zhang <a991391009@gmail.com>
2019-01-22 19:25:47 +0800

    sort and action list

---
+++ reference_book/chapter_three/squence_type.py
@@ -0,0 +1,26 @@
+def multiply(x):
+    return x * 3
+
+s = 'woshinide'
+print(list(s))
+l = []
+l.extend(list(s))
+print(l)
+print(l.count('i'))
+print(l.index('i'))
+l.insert(4, 'y')
+print(l)
+# print(l.insert(4,'y'))
+print(l.pop(1))
+print(l)
+print(l.remove('w'))
+print(l)
+
+print(l.reverse())
+print(l)
+num = [2,5,7,4,9,0]
+d = [{'name':'zhangmin','age':12},{'name':'wangsi','age':10},{'name':'limin','age':15}]
+d.sort(reverse=True,key=lambda x: x['age'])
+print(d)
+
+
