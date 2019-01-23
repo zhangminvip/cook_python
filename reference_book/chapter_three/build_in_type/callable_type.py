927fd9e11154098fdbec96971e171c9dc3f9a6fe
min.zhang <a991391009@gmail.com>
2019-01-23 19:13:20 +0800

    add callable type test

---
+++ reference_book/chapter_three/build_in_type/callable_type.py
@@ -0,0 +1,42 @@
+def foo(x, y):
+    return x + y
+
+print(foo.__code__)
+print(foo.__dict__)
+print(foo.__defaults__)
+print(foo.__globals__)
+print(foo.__closure__)
+
+
+class Foo(object):
+    def instance_method(self, arg):
+        print('instance method')
+
+    @classmethod
+    def class_method(cls, arg):
+        print('class method')
+
+    @staticmethod
+    def static_method(args):
+        print('static method')
+
+    def __call__(self, x):
+        print(2*x)
+
+
+
+f = Foo()
+print(id(f))   # decimal
+meth = f.instance_method   #python3: method object
+meth(37)
+umeth = Foo.instance_method # python3: function object has no attribute '__self__'
+umeth(f, 37)
+# umeth('haha',37)
+
+print(type(meth))
+print(meth.__name__, meth.__class__, meth.__func__, meth.__self__)  # hexadecimal
+print(type(umeth))
+
+print(len.__name__, type(len), len.__self__)
+
+f(2)
