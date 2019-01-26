68722366f35299d151a9f4ea0f29f30397ca4669
min.zhang <a991391009@gmail.com>
2019-01-26 21:13:31 +0800

    the difference between __getattribute__() and  __getattr__()

--- reference_book/chapter_three/object_comparison_sort/compare.py
+++ reference_book/chapter_three/object_comparison_sort/compare.py
@@ -14,6 +14,16 @@ python内置的一些类型中
 == 号自动调用__eq__函数。
 is 检查的是对象内存地址。
 
+
+__getattribute__()和 __getattr__()区别：
+
+1、如果某个类定义了 __getattribute__() 方法，
+在每次引用属性或方法名称时 Python 都调用它（特殊方法名称除外，因为那样将会导致讨厌的无限循环）。
+
+2、如果某个类定义了 __getattr__() 方法，Python 将只在正常的位置查询属性时才会调用它。
+如果实例 x 定义了属性 color， x.color 将 不会 调用x.__getattr__('color')；
+而只会返回 x.color 已定义好的值。
+
 '''
 
 class Example():
@@ -59,6 +69,11 @@ class Example():
         print('haha')
         return 'haha'
 
+    def __getattr__(self, item):
+        if item == 'val':
+            return 'attr val does not exist'
+        else:
+            raise AttributeError
 
 
 
@@ -76,4 +91,6 @@ print(t)
 t_result = max(t)
 print(t_result)
 
-print(isinstance(e, Example))
+# print(isinstance(e, Example))
+
+print(e.value)
